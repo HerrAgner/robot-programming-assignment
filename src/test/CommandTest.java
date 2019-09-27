7e5506564cf0d3bcf41603325ea963658b57bfba
Ted <ted.agner@hotmail.com>
2019-09-27 15:34:27 +0200

    CommandTest added

---
+++ src/test/CommandTest.java
@@ -0,0 +1,14 @@
+import main.java.com.robot.instructions.Command;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CommandTest {
+
+    @Test
+    public void getCommandFromShortCode() {
+        Assert.assertSame(Command.FORWARD, Command.getCommandFromChar('F'));
+        Assert.assertSame(Command.RIGHT, Command.getCommandFromChar('R'));
+        Assert.assertSame(Command.LEFT, Command.getCommandFromChar('L'));
+        Assert.assertNotSame(Command.FORWARD, Command.getCommandFromChar('L'));
+    }
+}
